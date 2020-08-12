bb83a541b84db93b15cf9caad85369c221672199
Luiz Henrique <luihsou1@gmail.com>
2020-08-12 01:57:25 -0300

    create use case update user

---
+++ src/users/use-cases/update-user/update-user.ts
@@ -0,0 +1,21 @@
+import { UseCase } from '@/shared/use-case';
+import { UserDto, User } from '@/users/entities/user';
+import { IUsersRepository } from '../../repositories/i-users-repository';
+
+/**
+ * Atualiza dados do usuário
+ */
+export class UpdateUser implements UseCase<UserDto, UserDto> {
+  constructor(
+    private readonly usersRepo: IUsersRepository,
+    private readonly ip: string,
+  ) {}
+
+  async execute(data?: UserDto): Promise<UserDto> {
+    data.ip = this.ip;
+    const user = await User.create(data);
+    await this.usersRepo.save(user);
+
+    return data;
+  }
+}
