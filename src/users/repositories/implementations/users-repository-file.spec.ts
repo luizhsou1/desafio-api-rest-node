c17fd72d80b098634c130cb96a543747b1dcb460
Luiz Henrique <luihsou1@gmail.com>
2020-08-12 18:15:17 -0300

    create repository files users

---
+++ src/users/repositories/implementations/users-repository-file.spec.ts
@@ -0,0 +1,51 @@
+import { jsonWriter } from '@/shared/files-helper';
+import { User, UserDto } from '../../../users/domain/entities/user';
+import { UsersRepositoryFile } from './users-repository-file';
+
+export const makeUserDtoFake = (): UserDto => ({
+  email: 'luiz@mail.com',
+  password: '123456',
+  fullName: 'Luiz Henrique de Souza',
+  dateOfBirth: '14/01/1997',
+  cpf: '39536580004',
+  rg: '12345678',
+  ip: '127.0.0.1',
+});
+
+describe('Repository - UsersRepositoryFile', () => {
+  // beforeEach(async () => {
+  //   jsonWriter('./src/shared/infra/files/users-tests.json', []);
+  // });
+
+  test('Deve salvar novo usuário', async () => {
+    const sut = new UsersRepositoryFile();
+
+    const fakeUserDto = makeUserDtoFake();
+    const u = await User.create(fakeUserDto);
+    await sut.save(u);
+
+    const user = await sut.findByEmail(fakeUserDto.email);
+    expect(user).toBeTruthy();
+  });
+
+  test('Deve atualizar informação de usuário existente', async () => {
+    const sut = new UsersRepositoryFile('./src/shared/files-test');
+
+    const fakeUserDto = makeUserDtoFake();
+    fakeUserDto.fullName = 'John';
+    const u = await User.create(fakeUserDto);
+    await sut.save(u);
+
+    const us = await sut.findByEmail(fakeUserDto.email);
+    expect(us.toDto().fullName).toBe('John');
+  });
+
+  test('Deve atualizar informação de usuário existente', async () => {
+    const sut = new UsersRepositoryFile('./src/shared/files-test');
+
+    const fakeUserDto = makeUserDtoFake();
+    fakeUserDto.fullName = 'John';
+    const u = await User.create(fakeUserDto);
+    await sut.saveTxt(u);
+  });
+});
--- src/users/repositories/implementations/users-repository-file.ts
+++ src/users/repositories/implementations/users-repository-file.ts
@@ -2,20 +2,61 @@
 // Escolhi trabalhar com JSON, única e exclusivamente por ser mais fácil de manipular no javascript
 // Mas tem o método saveTxt que persiti o usuário como txt no formato proposto no desafio, para poder ser baixado posteriormente
 
-import { User } from '@/users/domain/entities/user';
-import { NotFoundError } from '@/shared/errors';
+import { User, UserDto } from '@/users/domain/entities/user';
+import { jsonReader, jsonWriter, textWriter } from '@/shared/files-helper';
 import { IUsersRepository } from '../i-users-repository';
 
 export class UsersRepositoryFile implements IUsersRepository {
-  save(user: User): Promise<void> {
-    throw new Error('Method not implemented.');
+  private dirname: string;
+  constructor(dirname?: string) {
+    this.dirname = dirname || './src/shared/files';
+  }
+
+  async save(user: User): Promise<void> {
+    let users = jsonReader(`${this.dirname}/users.json`) as Array<UserDto>;
+    const userFound = await this.findByEmail(user.email.value);
+
+    if (userFound) {
+      users = users.map((u) => {
+        if (u.email === user.email.value) {
+          // Únicos dados que podem ser alterados
+          const { fullName, cpf, dateOfBirth, rg, ip } = user.toDto();
+          u.fullName = fullName;
+          u.cpf = cpf;
+          u.dateOfBirth = dateOfBirth;
+          u.rg = rg;
+          u.ip = ip;
+        }
+        return u;
+      });
+    } else {
+      users.push(user.toDto());
+    }
+
+    jsonWriter(`${this.dirname}/users.json`, users);
   }
 
   saveTxt(user: User): Promise<void> {
-    throw new Error('Method not implemented.');
+    const { email, cpf, dateOfBirth, fullName, rg, ip } = user.toDto();
+    const content = `Ex:
+Nome Completo: ${fullName}
+Data de Nascimento: ${dateOfBirth}
+CPF: ${cpf}
+RG: ${rg}
+
+Usuario Autenticado
+Login: ${email}
+IP: ${ip} (IP do último acesso)
+`;
+    textWriter(`${this.dirname}/${email}.txt`, content);
+    return Promise.resolve();
   }
 
-  findByEmail(email: string): Promise<User> {
-    throw new Error('Method not implemented.');
+  async findByEmail(email: string): Promise<User> {
+    const users = jsonReader(`${this.dirname}/users.json`) as Array<UserDto>;
+    const userDtoFound = users.length > 0 ? users.find((u) => u.email === email) : undefined;
+    if (userDtoFound) {
+      return await User.create(userDtoFound);
+    }
   }
 }
