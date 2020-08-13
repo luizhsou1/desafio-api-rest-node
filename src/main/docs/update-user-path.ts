1e5de80e4180725aec0534ef17ebd499c6da5f08
Luiz Henrique <luihsou1@gmail.com>
2020-08-13 09:17:31 -0300

    refactor docs

--- src/main/docs/update-user-path.ts
+++ src/main/docs/update-user-path.ts
@@ -5,7 +5,7 @@ export const updateUserPath = {
     }],
     tags: ['API'],
     summary: 'Endpoint privado para atualizar dados do usuário (clique no cadeado ao lado para informar token)',
-    description: 'Essa rota é privada e só pode ser executada por **usuários autenticados**',
+    description: 'Apenas o próprio usuário pode alterar seus dados, por isso não preciso que me passe na url algum parâmetro para identificá-lo, pego do próprio Jwt',
     parameters: [],
     requestBody: {
       required: true,
